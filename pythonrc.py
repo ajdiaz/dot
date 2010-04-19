da88089e5329d894150f65ea866aa5800e36a561
Andres J. Diaz <ajdiaz@connectical.com>
2010-04-19 20:29:20 +0200

    update pythonrc with adrian's modifications
     See more http://blogs.igalia.com/aperez/2010/04/make-the-python-interpreter-friendlier/

--- pythonrc.py
+++ pythonrc.py
@@ -1,11 +1,25 @@
 # vim:ft=python
-import sys
+import sys, os
 sys.ps1="[0;1m>>>[0;0m "
 sys.ps2="[0;1m...[0;0m "
+
 try:
-    import readline
-except ImportError:
-    sys.stderr.write("Module readline not available.\n")
-else:
-    import rlcompleter
+    import readline, atexit, os, rlcompleter
+ 
+    historypath = os.path.expanduser("~/.pyhistory")
     readline.parse_and_bind("tab: complete")
+ 
+    def save_history(historypath=historypath):
+        import readline
+        readline.write_history_file(historypath)
+ 
+    if os.path.exists(historypath):
+        readline.read_history_file(historypath)
+ 
+    atexit.register(save_history)
+ 
+    del os, atexit, readline, save_history, historypath, rlcompleter
+
+except:
+    pass
+
