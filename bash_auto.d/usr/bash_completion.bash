cfe17e331a0865908578011709ca86250d7a5971
Andres J. Diaz <ajdiaz@connectical.com>
2012-08-15 17:32:12 +0200

    Disable completion by default

--- bash_auto.d/usr/bash_completion.bash
+++ bash_auto.d/usr/bash_completion.bash
@@ -1,14 +1,16 @@
 # This file is sourced by .bashrc. This script load bash
 # completion functionality if it's enabled in the system.
 
-[ -f /etc/bash_completion ] && \
-	. /etc/bash_completion
+enable_completion () {
+	[ -f /etc/bash_completion ] && \
+		. /etc/bash_completion
 
-if [ -d ~/.bash_completion.d/ ]; then
-	for mod in $(ls -c1 ~/.bash_completion.d/) ; do
-		. ~/.bash_completion.d/$mod
-	done
-fi
+	if [ -d ~/.bash_completion.d/ ]; then
+		for mod in $(ls -c1 ~/.bash_completion.d/) ; do
+			. ~/.bash_completion.d/$mod
+		done
+	fi
+}
 
 # -- end -- vim:ft=sh:
 
