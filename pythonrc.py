5f340d64d29f0ee48f9cdce1bbc15368be7cfc7a
Andres J. Diaz <ajdiaz@connectical.com>
2014-09-28 17:36:50 +0200

    Add more python features

--- pythonrc.py
+++ pythonrc.py
@@ -1,25 +1,91 @@
 # vim:ft=python
-import sys, os
-sys.ps1="[0;1m>>>[0;0m "
-sys.ps2="[0;1m...[0;0m "
 
+import os
+import sys
+import pprint
+
+from code import InteractiveConsole
+from tempfile import mkstemp
+
+# Color Support
+base = '\001\033[%sm\002'
+color = {
+    "reset": base % "38;5;7",
+    "blue": base % "38;5;104",
+    "white": base % "38;5;15",
+}
+
+sys.ps1 = '%s>>> %s' % (color["blue"], color["reset"])
+sys.ps2 = '%s... %s' % (color["white"], color["reset"])
+
+
+# Fancy display variables
+def my_displayhook(value):
+    if value is not None:
+        try:
+            import __builtin__
+            __builtin__._ = value
+        except ImportError:
+            __builtins__._ = value
+
+        pprint.pprint(value)
+
+
+sys.displayhook = my_displayhook
+
+# Historic
 try:
-    import readline, atexit, os, rlcompleter
- 
+    import readline
+    import atexit
+    import rlcompleter
+
     historypath = os.path.expanduser("~/.pyhistory")
     readline.parse_and_bind("tab: complete")
- 
+
     def save_history(historypath=historypath):
         import readline
         readline.write_history_file(historypath)
- 
+
     if os.path.exists(historypath):
         readline.read_history_file(historypath)
- 
+
     atexit.register(save_history)
- 
-    del os, atexit, readline, save_history, historypath, rlcompleter
+
+    del atexit, readline, rlcompleter
 
 except:
     pass
 
+# Fancy inline editor
+EDITOR = os.environ.get('EDITOR', 'vi')
+EDIT_CMD = '\e'
+
+
+class EditableBufferInteractiveConsole(InteractiveConsole):
+    def __init__(self, *args, **kwargs):
+        self.last_buffer = []  # This holds the last executed statement
+        InteractiveConsole.__init__(self, *args, **kwargs)
+
+    def runsource(self, source, *args):
+        self.last_buffer = [source.encode('utf-8')]
+        return InteractiveConsole.runsource(self, source, *args)
+
+    def raw_input(self, *args):
+        line = InteractiveConsole.raw_input(self, *args)
+        if line == EDIT_CMD:
+            fd, tmpfl = mkstemp('.py')
+            os.write(fd, b'\n'.join(self.last_buffer))
+            os.close(fd)
+            os.system('%s %s' % (EDITOR, tmpfl))
+            line = open(tmpfl).read()
+            os.unlink(tmpfl)
+            tmpfl = ''
+            lines = line.split('\n')
+            for i in range(len(lines) - 1):
+                self.push(lines[i])
+            line = lines[-1]
+        return line
+
+c = EditableBufferInteractiveConsole(locals=locals())
+c.interact(banner='')
+sys.exit(0)
