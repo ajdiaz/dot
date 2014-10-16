e49d34b370269ca97e08a9fe3373c6dd891f1558
Andres J. Diaz <ajdiaz@connectical.com>
2014-10-16 10:23:07 +0200

    Improve pythonrc behaviour

--- pythonrc.py
+++ pythonrc.py
@@ -33,6 +33,23 @@ def my_displayhook(value):
 
 sys.displayhook = my_displayhook
 
+
+def info(type, value, tb):
+    if type != SyntaxError and hasattr(sys, 'ps1') or not sys.stderr.isatty():
+        # we are in interactive mode or we don't have a tty-like
+        # device, so we call the default hook
+        import traceback
+        import pdb
+        # we are NOT in interactive mode, print the exception
+        traceback.print_exception(type, value, tb)
+        print
+        # then start the debugger in post-mortem mode.
+        # pdb.pm() # deprecated
+        pdb.post_mortem(tb)  # more modern
+    else:
+        sys.__excepthook__(type, value, tb)
+sys.excepthook = info
+
 # Historic
 try:
     import readline
