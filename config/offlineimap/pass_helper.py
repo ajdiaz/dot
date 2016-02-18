f27891c3fc8d7335d2ef124aef16a7baef92a9fd
Andrés J. Díaz <ajdiaz@connectical.com>
2016-02-18 16:18:08 +0100

    Use pass for passwords in offlineimap

---
+++ config/offlineimap/pass_helper.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import subprocess
+
+PASS = "pass"
+
+
+def get_credentials(keyname):
+    proc = subprocess.Popen([PASS, "show", keyname],
+                            stdout=subprocess.PIPE)
+    output = proc.stdout.readlines()
+    if not output:
+        raise ValueError("Credential not found")
+    else:
+        password = output[0].strip()
+        username = output[1].split(":")[1].strip()
+
+    return (username, password)
+
+
+def get_username(keyname):
+    return get_credentials(keyname)[0]
+
+def get_password(keyname):
+    return get_credentials(keyname)[1]
