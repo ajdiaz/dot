9799ab9f37ab4e76d3b7509681a7b8d890ed6d3f
Andrés J. Díaz <ajdiaz@connectical.com>
2016-10-07 12:35:25 +0200

    Avoid repeting new mail messages

--- config/offlineimap/notify.sh
+++ config/offlineimap/notify.sh
@@ -1,7 +1,17 @@
-#! /bin/sh
+#! /bin/bash
 
-query="tag:unread"
-new="$(notmuch search --output=files $query | wc -l)"
+INTERVAL=180
 
-[ $new -gt 0 ] && notify-send -u low -t 10 "Received: $new emails"
+declare -a unread=( $(notmuch search --output=files tag:unread) )
+declare -a new=()
+
+now="$(printf '%(%s)T\n')"
+
+for item in "${unread[@]}"; do
+  mtime="$(stat -c '%Y' "$item")"
+  [[ $((now-mtime)) -le "$INTERVAL" ]] && new+=( "$item" )
+done
+
+
+[ "${#new[@]}" -gt 0 ] && notify-send -u low -t 10 "Received: ${#new[@]} emails"
 true
