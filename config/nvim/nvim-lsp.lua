e4b877b44d61935b1c51a0128c33dde2d72893fb
Andrés J. Díaz <ajdiaz@ajdiaz.me>
2023-03-04 18:57:36 +0100

    chore: update modules
    Signed-off-by: Andrés J. Díaz <ajdiaz@ajdiaz.me>

--- config/nvim/nvim-lsp.lua
+++ config/nvim/nvim-lsp.lua
@@ -14,7 +14,7 @@ end
 
 -- Set Capabilities.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 vim.diagnostic.config({
   virtual_text = {
